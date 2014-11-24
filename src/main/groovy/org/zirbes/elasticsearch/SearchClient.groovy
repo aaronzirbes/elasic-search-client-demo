879306a03c6c826144cc34532d86cd9623ddf3dd
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2014-11-23 22:29:01 -0600

    Adding search query

--- src/main/groovy/org/zirbes/elasticsearch/SearchClient.groovy
+++ src/main/groovy/org/zirbes/elasticsearch/SearchClient.groovy
@@ -2,12 +2,26 @@ package org.zirbes.elasticsearch
 
 import com.fasterxml.jackson.databind.ObjectMapper
 
+import static org.elasticsearch.index.query.QueryBuilders.*
+import static org.elasticsearch.index.query.FilterBuilders.*
+
 import groovy.transform.CompileStatic
 
 import org.elasticsearch.action.delete.DeleteResponse
 import org.elasticsearch.action.get.GetResponse
 import org.elasticsearch.action.index.IndexResponse
+import org.elasticsearch.action.search.SearchResponse
+import org.elasticsearch.action.search.SearchType
 import org.elasticsearch.client.Client
+import org.elasticsearch.common.geo.ShapeRelation
+import org.elasticsearch.index.query.QueryBuilder
+import org.elasticsearch.search.SearchHit
+import org.elasticsearch.search.SearchHits
+
+
+import com.spatial4j.core.context.SpatialContext
+import com.spatial4j.core.shape.Shape
+import com.spatial4j.core.shape.impl.RectangleImpl
 
 @CompileStatic
 class SearchClient {
@@ -38,6 +52,9 @@ class SearchClient {
         Thing copyOfThing = getThing(thing.key)
         println "Got thing: ${copyOfThing.key}"
 
+        List<Thing> foundThings = findThing('Minneapolis')
+        println "Found things: ${foundThings}"
+
         deleteThing thing.key
         println "Deleted thing: ${thing.key}"
 
@@ -48,12 +65,39 @@ class SearchClient {
         client.close()
     }
 
-    void showResults(IndexResponse response) {
+    private void showIndex(IndexResponse response) {
         println "    Index name ${response.index}"
         println "    Type name ${response.type}"
         println "    Document ID (generated or not) ${response.id}"
         println "    Version (if it's the first time you index this document, you will get: 1) ${response.version}"
     }
+
+    private List<Thing> findThing(String query) {
+
+        QueryBuilder builder = boolQuery().must(termQuery('city', query.toLowerCase()))
+        // QueryBuilder builder = termQuery('city', query.toLowerCase())
+
+        println "  SEARCH QUERY >> \n${builder}"
+        println ''
+
+        SearchResponse response = client.prepareSearch(INDEX_NAME)
+                                        .setTypes(DATA_TYPE)
+                                        .setQuery(builder)
+                                        .setExplain(true)
+                                        .setSize(10)
+                                        .execute()
+                                        .actionGet()
+
+        SearchHits hitContainer = response.hits
+        println "total hits: ${hitContainer.totalHits}"
+
+        List<SearchHit> hits = hitContainer.hits as List
+
+        List<String> responses = hits.collect{ SearchHit hit -> hit.sourceAsString() }
+
+        return responses.collect{ String it -> fromJson(it) }
+    }
+
 
     private String putThing(Thing thing) {
 
@@ -64,7 +108,7 @@ class SearchClient {
                                        .execute()
                                        .actionGet()
 
-        showResults response
+        showIndex response
 
         return response.id
     }
@@ -77,13 +121,21 @@ class SearchClient {
 
         if (response.exists) {
             String json = response.sourceAsString
-            Thing thing = mapper.readValue(json, Thing)
+            Thing thing = fromJson(json)
             println "Thing :: ${thing}"
             return thing
         }
         return null
     }
 
+    // Coordinates: Minnesota?
+    private String locationQuery(Integer miles) {
+        return geoShapeQuery('location', state, 'states')
+
+        // Use filter ?
+        // GeoShapeFilterBuilder.geoShapeFilter(String name, ShapeBuilder shape, ShapeRelation.DISJOINT)
+        // .relation(ShapeRelation.DISJOINT)
+    }
 
     private void deleteThing(String key) {
         DeleteResponse response = client.prepareDelete(INDEX_NAME, DATA_TYPE, key)
@@ -96,8 +148,12 @@ class SearchClient {
         // TODO
     }
 
-    private String toJson(Object object) {
-        return mapper.writeValueAsString(object)
+    private String toJson(Thing thing) {
+        return mapper.writeValueAsString(thing)
+    }
+
+    private Thing fromJson(String json) {
+        return mapper.readValue(json, Thing)
     }
 
     private String getJsonFromResource(String path) {
